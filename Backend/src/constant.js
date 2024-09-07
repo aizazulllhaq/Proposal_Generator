9f945f2bfa0cd8bac92b8c36dc37f5aa55d13dff
aizazulllhaq <aizazulllhaq@gmail.com>
2024-09-06 21:41:58 -0700

    Initial Commit

---
+++ Backend/src/constant.js
@@ -0,0 +1,11 @@
+export const SERVER_URL = process.env.SERVER_URL;
+export const PORT = process.env.PORT;
+
+export const MONGO_URL = process.env.MONGO_URL;
+export const DB_NAME = process.env.DB_NAME;
+
+export const JWT_SECRET = process.env.JWT_SECRET;
+
+export const FRONTEND_URL = process.env.FRONTEND_URL;
+
+export const FPORT = process.env.FPORT;
